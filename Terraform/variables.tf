ea600b29851155b85e1028e5c0581485e62b9b14
Akshay Shinde <159605371+akshayshinde3@users.noreply.github.com>
2024-06-18 11:40:25 +0530

    Update variables.tf

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,12 +1,12 @@
 # Define variables
 variable "aws_region" {
   description = "AWS region where resources will be provisioned"
-  default     = "us-east-1"
+  default     = "us-east-2"
 }
 
 variable "ami_id" {
   description = "AMI ID for the EC2 instance"
-  default     = "ami-04b70fa74e45c3917"
+  default     = "ami-09040d770ffe2224f"
 }
 
 variable "instance_type" {
