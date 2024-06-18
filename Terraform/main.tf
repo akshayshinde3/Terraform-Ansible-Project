68367c28dcf66b041a23349f2edc52667e33b09d
Akshay Shinde <159605371+akshayshinde3@users.noreply.github.com>
2024-06-18 11:38:39 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,14 +1,14 @@
 
 # Define your resources, such as EC2 instances, S3 buckets, etc.
-resource "aws_instance" "sudha-terraform" {
+resource "aws_instance" "Akshay-terraform" {
   ami           = var.ami_id
   instance_type = var.instance_type
-  key_name      = "sudha_key"
+  key_name      = "akshay"
   count = 2
-  security_groups = ["launch-wizard-7"]
+  security_groups = ["launch-wizard-1"]
 
   tags = {
     Environment = "dev"
-    Name = "sudha-terraform_${count.index + 1}"
+    Name = "Akshay-terraform_${count.index + 1}"
   }
 }
